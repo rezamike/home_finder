922c365661c87bc7970e0b3baf4218ce8ea8dc8d
rezamike <michael.sanaiha@gmail.com>
2024-04-02 01:20:01 -0700

    init commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div className="w-full p-6">
+      <Navbar />
+      <Outlet />
+    </div>
+  );
+};
+export default App;
