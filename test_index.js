ff241d49f1b148942affee0eeef69ef1f1453afa
rezamike <michael.sanaiha@gmail.com>
2024-04-07 00:28:38 -0700

    fixed test index node for pulling site data

--- test_index.js
+++ test_index.js
@@ -1,61 +1,73 @@
-const request = require("request");
 const express = require("express");
-const cheerio = require("cheerio");
 const axios = require("axios");
+const cheerio = require("cheerio");
+
 const app = express();
+const port = 5173;
 
-app.use(express.json());
+const headers = {
+  AUTHORIZATION: "c479a87e-e910-4730-9c58-df3a7b54ae11",
+  "user-agent": "null",
+};
 
-app.listen(5173, () => {
-  console.log("server started on 5173");
+async function scrape_page(url) {
+  try {
+    const response = await axios.get(
+      `https://recorder.maricopa.gov/recdocdata/${url}`,
+      { headers }
+    );
+    const $ = cheerio.load(response.data);
+
+    const link_main = [];
+    //config select
+    $(".main_area table td").each((i, element) => {
+      if ($(element).find("a").attr("href") != null) {
+        const link = $(element).find("a").attr("href");
+        link_main.push("https://recorder.maricopa.gov/recdocdata/" + link);
+      }
+    });
+    const scraped_data = {
+      final_link: link_main,
+    };
+    return scraped_data;
+  } catch (error) {
+    console.error("Error scraping page:", error);
+    return null;
+  }
+}
+
+app.get("/", async (req, res) => {
+  try {
+    const initial_response = await axios.get(
+      "https://recorder.maricopa.gov/recdocdata/GetRecDataRecentPgDn.aspx?rec=0&suf=&nm=&bdt=1%2f1%2f2024&edt=1%2f31%2f2024&cde=NS&max=20&res=True&doc1=NS&doc2=&doc3=&doc4=&doc5=",
+      { headers }
+    );
+    const $ = cheerio.load(initial_response.data);
+
+    const links = [];
+    //config main
+    $(".main_area table tr").each((i, element) => {
+      if ($(element).find("a").attr("href") != null) {
+        const link = $(element).find("a").attr("href");
+        links.push(link);
+      }
+    });
+
+    const scraped_data = [];
+    for (const link of links) {
+      const data = await scrape_page(link);
+      if (data) {
+        scraped_data.push(data);
+      }
+    }
+
+    res.json(scraped_data);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
-const config_main = {
-  method: "get",
-  maxBodyLength: Infinity,
-  url: "https://recorder.maricopa.gov/recdocdata/GetRecDataRecentPgDn.aspx?rec=0&suf=&nm=&bdt=1%2f1%2f2024&edt=1%2f31%2f2024&cde=NS&max=20&res=True&doc1=NS&doc2=&doc3=&doc4=&doc5=",
-  headers: {
-    AUTHORIZATION: "c479a87e-e910-4730-9c58-df3a7b54ae11",
-    "user-agent": "null",
-  },
-};
-
-const config_select = {
-  method: "get",
-  maxBodyLength: Infinity,
-  url: "https://recorder.maricopa.gov/recdocdata/GetRecDataRecentDetail.aspx?rec=20240000140",
-  headers: {
-    AUTHORIZATION: "c479a87e-e910-4730-9c58-df3a7b54ae11",
-    "user-agent": "null",
-  },
-};
-
-app.get("/data", function (req, res) {
-  axios
-    .request(config)
-    .then((response) => {
-      const links = [];
-      const $ = cheerio.load(response.data);
-
-      //config select
-      // $(".main_area table td").each((i, element) => {
-      //   if ($(element).find("a").attr("href") != null) {
-      //     const link = $(element).find("a").attr("href");
-      //     links.push(link);
-      //   }
-      // });
-
-      //config main
-      $(".main_area table tr").each((i, element) => {
-        if ($(element).find("a").attr("href") != null) {
-          const link = $(element).find("a").attr("href");
-          links.push(link);
-        }
-      });
-
-      res.send(links);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
